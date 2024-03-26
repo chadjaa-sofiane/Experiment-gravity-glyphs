5b95650114f71cf7df88001ce5912a24da403843
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-26 21:12:56 +0100

    Install three.js.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import * as THREE from "three";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+camera.position = 1;
+
 const App = () => {
-  return <div>App</div>;
+  return <div></div>;
 };
 
 export default App;
